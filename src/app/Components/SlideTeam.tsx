ffa98c9534a0aff08aeef0d73485a4d5f7842270
Ade <85540115+yorisofficial@users.noreply.github.com>
2024-06-03 20:44:25 +0800

    Update : Data file

--- src/app/Components/SlideTeam.tsx
+++ src/app/Components/SlideTeam.tsx
@@ -1,10 +1,10 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import {Autoplay} from 'swiper/modules'
-import {Swiper, SwiperSlide} from 'swiper/react'
-import {CoreTeam} from '../lib/primary/CoreTeam'
+"use client"
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
+import {Autoplay} from "swiper/modules"
+import {Swiper, SwiperSlide} from "swiper/react"
+import {CoreTeam} from "../lib/primary/CoreTeam"
 export const calculateAge = (birthYear: number) => {
   const age = new Date().getFullYear() - birthYear
   return age
@@ -14,7 +14,7 @@ const SlideTeam = () => {
   const data = CoreTeam
 
   return (
-    <div className='list-coach'>
+    <div className="list-coach">
       <Swiper
         modules={[Autoplay]}
         spaceBetween={32}
@@ -31,37 +31,37 @@ const SlideTeam = () => {
             slidesPerView: 3,
           },
         }}
-        className='list-coach mySwiper'
+        className="list-coach mySwiper"
       >
         {data.map((item) => (
-          <SwiperSlide key={item.id} className='item-coach h-full'>
-            <div className='item-coach group h-full min-h-[400px] w-full cursor-pointer rounded-xl duration-500'>
+          <SwiperSlide key={item.id} className="item-coach h-full">
+            <div className="item-coach group h-full min-h-[400px] w-full cursor-pointer rounded-xl duration-500">
               <Image
                 src={item.profile}
                 width={500}
                 height={500}
-                alt='..'
-                className='h-full min-h-[400px] w-[300px] rounded-xl object-cover duration-500'
+                alt=".."
+                className="h-full min-h-[400px] w-[300px] rounded-xl object-cover duration-500"
               />
 
-              <div className='profile-coach absolute bottom-4 left-4 text-white'>
-                <div className='flex flex-col items-start justify-start gap-4'>
-                  <div className='text-start'>
-                    <div className='flex items-center gap-2'>
-                      <h1 className='coach-name text-start text-2xl font-black '>
+              <div className="profile-coach absolute bottom-4 left-4 text-white">
+                <div className="flex flex-col items-start justify-start gap-4">
+                  <div className="text-start">
+                    <div className="flex items-center gap-2">
+                      <h1 className="coach-name text-start text-2xl font-black ">
                         {item.full_name}
                       </h1>
-                      <span className='text-base font-light'>
+                      <span className="text-base font-light">
                         | {calculateAge(item?.age)}
                         y.o
                       </span>
                     </div>
-                    <span className='text-sm'>+10 Year coach experienced</span>
+                    <span className="text-sm">+10 Year coach experienced</span>
                   </div>
-                  <div className=''>
+                  <div className="">
                     <Link
-                      href={`/team/team/${item.full_name.replaceAll(' ', '-').toLocaleLowerCase()}`}
-                      className='text-sm underline underline-offset-4'
+                      href={`/team/team/${item.full_name.replaceAll(" ", "-").toLocaleLowerCase()}`}
+                      className="text-sm underline underline-offset-4"
                     >
                       See profile
                     </Link>
--- src/app/lib/primary/CoreTeam.ts
+++ src/app/lib/primary/CoreTeam.ts
@@ -7,20 +7,32 @@ export const CoreTeam = [
     level: "ISA Lv1",
     quotes:
       "Surfing development has been a significant part of my professional life. I was a professional surfer throughout my life, traveling around the globe and competing since a young age. In 2001, I contributed to the development of surfing by creating the Indonesian Surfing Championship. This initiative evolved into the Asian Surfing Championship in 2008, with our company accelerating the development of surfing in Indonesia and beyond through the ASC since 2004. Additionally, I have been involved in youth movement by creating the Grom Patrol surf series since 2019. I also played a role in helping Indonesia secure gold medals in the Sea Games in Manila and assisted the first Indonesian surfer to qualify for the Tokyo Olympics.With all this experience, ASPA will be the channel to help every potential athlete achieve their goals by providing guidance and strategy.",
-    age: 1982,
+    age: 1977,
     profile: "/assets/team/tipi-jabrik.png",
     user_instagram: "https://www.instagram.com/tipijabrik",
     user_soorfinc: "Tipi Jabrik",
-    experience: "+10 years coach",
+    experience: "+15 years coach",
     archievement: [
       {
-        title: "",
+        title: "2024 Junior National Coach, El Salvador",
       },
       {
-        title: "item 2",
+        title: "2021 National Coach, Olympic Tokyo",
       },
       {
-        title: "item 3",
+        title: "2017 National Coach, Sea Games Manila",
+      },
+      {
+        title: "2010 Junior National Coach, New Zealand",
+      },
+      {
+        title: "2018 Quiksilver Uluwatu Challenge Champion",
+      },
+      {
+        title: "2010 Quiksilver Phuket Champion",
+      },
+      {
+        title: "2001 CHP Challenge Japan Champion",
       },
     ],
   },
