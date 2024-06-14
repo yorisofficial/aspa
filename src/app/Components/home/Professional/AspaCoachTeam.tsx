bc6c6f68d94ba3a040150c7f3b2b4be3ba694138
Ade <85540115+yorisofficial@users.noreply.github.com>
2024-06-14 10:55:25 +0800

    Update AspaCoachTeam.tsx

--- src/app/Components/home/Professional/AspaCoachTeam.tsx
+++ src/app/Components/home/Professional/AspaCoachTeam.tsx
@@ -24,9 +24,15 @@ const AspaCoachTeam = ({
   categories: string
 }) => {
   const data = teamData
+
   return (
     <div className="simple-container border-buttom flex flex-col items-start justify-start gap-8 py-8">
-      <h1 className="text-3xl font-black">{title}</h1>
+      <h1 className="text-3xl font-black">
+        {title} |{" "}
+        <small className="font-light">
+          Total : <span className="font-black">{data.length}</span>
+        </small>
+      </h1>
       <Suspense fallback={<Loading />}>
         <ListTeam data={data} categories={categories} />
       </Suspense>
