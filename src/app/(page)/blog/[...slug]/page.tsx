b15ad2e6ed753572c63f61f8cd67dd5e93f94781
Ade <85540115+yorisofficial@users.noreply.github.com>
2024-07-09 22:29:45 +0800

    Update blog section

--- src/app/(page)/blog/[...slug]/page.tsx
+++ src/app/(page)/blog/[...slug]/page.tsx
@@ -45,22 +45,24 @@ const BlogPage = async ({ params }: { params: { slug: string } }) => {
         Back to main
       </BackButton>
       <div className="mb-8 rounded-xl border-2 p-4">
-        <div className="h-[300px] w-full max-w-5xl md:h-full xl:w-[400px]">
+        <div className="h-full w-full md:h-[300px] md:w-full">
           <Image
             src={data.jetpack_featured_media_url}
             height={1500}
             width={1500}
             priority={false}
             alt={data.title.rendered}
-            className="h-full w-[300px] rounded-lg object-cover md:w-full"
+            className="h-full w-full rounded-lg object-cover"
           />
         </div>
         <div className="py-4">
           <GetDateComponent data={data.date} />
-          <h1 className="w-3/4 text-xl font-black">{data.title.rendered}</h1>
+          <h1 className="w-full text-xl font-black md:w-3/4">
+            {data.title.rendered}
+          </h1>
         </div>
       </div>
-      <article className="prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-figcaption: prose max-w-none prose-headings:font-black prose-p:text-sm prose-p:text-black prose-figure:!w-full prose-figcaption:w-full prose-figcaption:text-balance prose-strong:text-primary prose-img:w-[300px] prose-img:max-w-5xl prose-img:rounded-xl xl:prose-img:w-full">
+      <article className="prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-figcaption: prose max-w-none prose-headings:font-black prose-p:text-sm prose-p:text-black prose-figure:!w-full prose-figcaption:w-full prose-figcaption:text-balance prose-strong:text-primary prose-img:w-full prose-img:max-w-5xl prose-img:rounded-xl md:prose-img:w-full">
         <div dangerouslySetInnerHTML={{ __html: data.content.rendered }} />
       </article>
     </ContainerContent>
