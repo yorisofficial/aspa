fe84ea650c6de290fdd270987bd38c17d840e319
Ade <85540115+yorisofficial@users.noreply.github.com>
2024-07-18 15:54:08 +0800

    Update the image about us

--- src/app/Components/home/Program/OurProgram.tsx
+++ src/app/Components/home/Program/OurProgram.tsx
@@ -7,14 +7,15 @@ import { GromPatrol } from "@/app/lib/events/grompatrol";
 const OurProgram = () => {
   return (
     <ContainerContent url="/program" className="border-buttom py-8">
-      <div className="relative h-full w-full xl:max-w-5xl">
+      <div className="relative h-full w-full space-y-6 xl:max-w-5xl">
         <Image
           src={"/assets/hero-img-1.webp"}
-          width={1500}
-          height={1500}
+          width={1000}
+          height={500}
           priority={false}
+          quality={75}
           alt="ASPA Logo"
-          className="hidden h-[440px] w-full rounded-xl object-cover md:inline-block"
+          className="h-[250px] w-full rounded-xl object-cover md:inline-block md:h-[440px]"
         />
         <div className="program-card inline-block space-y-4 rounded-lg bg-primary p-6 text-white xl:absolute xl:left-6 xl:top-1/2 xl:w-1/2 xl:-translate-y-1/2 xl:p-8">
           <div className="absolute bottom-0 right-0 -z-10">
@@ -27,22 +28,13 @@ const OurProgram = () => {
             />
           </div>
           <div className="header">
-            <h1 className="text-base font-bold uppercase xl:text-xl">
+            <h1 className="text-lg font-bold uppercase xl:text-xl">
               {GromPatrol.title}
             </h1>
           </div>
           <p className="w-full text-justify text-sm font-light md:text-base">
             {GromPatrol.desc}
           </p>
-          {/* <div className="flex w-full flex-shrink-0 items-center gap-4">
-            <ButtonBooking invert={true} data={urlBooking} />
-            <Link
-              href={"/program"}
-              className="w-fit text-sm underline underline-offset-4"
-            >
-              About the program
-            </Link>
-          </div> */}
         </div>
       </div>
     </ContainerContent>
