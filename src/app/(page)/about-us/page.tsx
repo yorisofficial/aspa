f203a3d8a57fb3fb6786faa3b9882a056e3b8c74
ADE YOGA <85540115+yorisofficial@users.noreply.github.com>
2025-01-29 14:52:46 +0700

    Update about page & navigation

--- src/app/(page)/about-us/page.tsx
+++ src/app/(page)/about-us/page.tsx
@@ -2,10 +2,12 @@ import AccordionAboutUs from "@/app/Components/AccordionAboutUs";
 import { ContentData } from "@/app/lib/Content";
 import { AllTeams } from "@/app/lib/primary/AllTeams";
 import Loading from "@/app/loading";
-import { InstagramLogo } from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
+import HeaderSection from "./components/HeaderSection";
+import HistoricalContent from "./components/HistoricalContent";
+import TeamsPreviews from "./components/TeamsPreviews";
 
 export const metadata = {
   title: "About Us",
@@ -42,82 +44,36 @@ export const metadata = {
 const AcademyPage = () => {
   // get content about us
   const contentText = ContentData[0];
-  const teams = AllTeams;
   const motoBrand = [
     {
       title: "Vision",
       description:
-        "Support Surf Athletes to become the worlds best surfers and people",
+        "To be a global leader in surf performance and education, inspiring individuals to achieve excellence, embrace the ocean responsibly, and contribute to the growth and sustainability of surfing worldwide.",
     },
     {
       title: "Mision",
       description:
-        "Transform into a Performance Surf Academy that prioritizes athletes, is supported by coaches, focuses on performance, and integrates all aspects of performance",
+        "To empower surfers of all levels through high-quality coaching, surf education, and professional development, fostering a community that values performance, sustainability, and growth both in and out of the water.",
     },
   ];
   return (
     <>
-      <div className="min-h-screen w-full">
-        <div className="section-header flex h-fit w-full flex-col gap-4 px-4 md:items-center md:justify-center md:px-10 xl:px-0">
-          <div className="core-content flex w-full flex-col md:items-center md:text-center">
-            <span className="font-bold uppercase text-brand">
-              ASIAN SURF PERFORMANCE ACADEMY
-            </span>
-            <h2 className="big-heading text-xl uppercase md:w-3/4 md:text-3xl xl:w-1/2 xl:text-4xl">
-              Pushing Boundaries, Sharpening Skills, Becoming the Best Surfer!
-            </h2>
-          </div>
-          <p className="text-justify text-base md:text-center xl:w-3/4">
-            {contentText.description}
-          </p>
-          <Link
-            href={"/"}
-            target="_blank"
-            aria-label="Link to instagram"
-            className="group flex w-fit items-center justify-center rounded-lg border-[0.5px] border-black p-1 duration-500 hover:border-brand"
-          >
-            <InstagramLogo
-              size={24}
-              weight="fill"
-              className="duration-500 group-hover:text-brand"
-            />
-          </Link>
-        </div>
-        <div className="image-width mt-4 h-[200px] w-full md:mt-6 md:h-[300px] xl:mt-8">
+      <div className="min-h-screen w-full overflow-hidden pt-8">
+        <HeaderSection
+          title={contentText.quote}
+          about={contentText.description}
+        />
+        <div className="image-width mt-4 h-[200px] w-full overflow-hidden md:mt-6 md:h-[400px] xl:mt-8">
           <Image
-            src={
-              "https://s3-alpha-sig.figma.com/img/c966/d565/cff55a4f8e18028e615d7c27b7b5da52?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gJkR-Dm8N5p19nuRkVuJj8aPdDeLLqTScNnXCsrKaTsozcP0WsYsjOeyzRyxNCSukzjk4IY10PzXlu~oLl6fnDll-pqdAezjIfmpHaKweMwTaUhzs94Kz0XRyaLQtuqhrGJ77b6VVHhku-IYNOZDRF9eFxvy6PJFz7E4jqiFGqsx7gHMDCFwt8W4Tlldn3SsnocVzgZbz6LQ9eNsfR3zUar856PwF9Z09Mm9HRXgM~UF37EXipp14fFIrPAsqlZi1ZZf6~VUECQI7GQRwrp9RDDhL0N11gcJETP3cTCADwdoQql15OcrsDjf5d0v~rYNtMY9pFfBhPLteMcVoiqBog__"
-            }
+            src={"/Dump/WhatsApp Image 2025-01-29 at 14.21.31_fd7b41db.jpg"}
             alt="image"
             width={2400}
-            height={800}
-            className="h-full w-full object-cover"
+            height={1000}
+            priority
+            className="h-[200px] w-full object-cover md:h-[400px]"
           />
         </div>
-        <div className="our-teams mx-auto mt-4 flex h-fit w-full max-w-6xl flex-col items-start justify-between px-4 md:mt-6 md:flex-row md:gap-5 md:px-10 xl:mt-8 xl:px-0">
-          <div className="core-content md:w-3/4">
-            <h2 className="big-heading text-2xl uppercase md:text-4xl">
-              Where ASC&apos;s Expertise Meets Your Surfing Ambitions
-            </h2>
-          </div>
-          <p className="mt-4 w-full text-justify text-base md:mt-0">
-            Behind and supporting ASPA is the Asian Surf Cooperative or ASC,
-            with over 20 years of experience in the surf industry. As a
-            professional surfing organization, ASC has nearly two decades of
-            experience in organizing, managing, and supporting surfing events
-            across the Asian Region, from local boardriders club competitions to
-            World Championship Tour events, including the recent 2022 Quiksilver
-            Pro G-Land. Bali offers everything a surfer needs to develop and
-            improve their skills to become one of the best surfers in the world.
-            With a wide variety of waves, both reef and sand, and a vibrant
-            surfing community, ASPA provides a specialized space for surfers to
-            enjoy and learn. With top coaches and trainers, athletes have access
-            to various activities and training sessions. At ASPA, we aim to
-            provide the best performance experience for the best surfers,
-            offering fully equipped facilities and an environment tailored for
-            success.
-          </p>
-        </div>
+        <HistoricalContent />
         <div className="moto-section tems-start mx-auto mt-4 flex h-fit w-full max-w-6xl flex-col justify-between px-4 md:mt-6 md:gap-5 md:px-10 xl:px-0">
           <Suspense fallback={<Loading />}>
             {motoBrand.map((item, index) => (
@@ -129,55 +85,8 @@ const AcademyPage = () => {
             ))}
           </Suspense>
         </div>
-        <div className="our-teams mx-auto mt-4 h-fit w-full max-w-6xl px-4 md:mt-6 md:px-10 md:text-center xl:mt-8 xl:px-0">
-          <h2 className="big-heading text-2xl uppercase md:text-center md:text-4xl">
-            meet everyone who made this posible
-          </h2>
-          <div className="content-list h-fit w-full">
-            <div className="mt-4 grid h-fit w-full grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4">
-              {teams.splice(0, 8).map((item) => (
-                <div key={item.id} className="group">
-                  <div className="img-profile h-[200px] w-full overflow-hidden rounded-lg">
-                    <Image
-                      src={item.profile}
-                      alt="profile"
-                      width={500}
-                      height={500}
-                      priority
-                      className="h-full w-full rounded-lg object-cover duration-500 group-hover:scale-110"
-                    />
-                  </div>
-                  <div className="body-data mt-2 flex flex-col items-center justify-center">
-                    <h2 className="text-center text-base font-bold uppercase md:text-xl">
-                      {item.full_name}
-                    </h2>
-                    <span className="text-center text-sm font-light">
-                      {item.title}
-                    </span>
-                    <Link
-                      target="_blank"
-                      aria-label={`Instagram ${item.full_name}`}
-                      href={item.user_instagram}
-                      className="mt-2 block"
-                    >
-                      <InstagramLogo size={24} />
-                    </Link>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="mt-4 flex w-full items-center justify-center">
-              <Link
-                href="/team"
-                aria-label="See more teams"
-                className="flex items-center gap-2 rounded bg-black px-4 py-2 text-white"
-              >
-                See more
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="image-width mt-4 h-[200px] w-full md:mt-6 md:h-[300px] xl:mt-8">
+        <TeamsPreviews />
+        <div className="image-width mt-4 h-[200px] w-full md:mt-6 md:h-[400px] xl:mt-8">
           <Image
             src={
               "https://s3-alpha-sig.figma.com/img/3e9c/a1b7/31529b4fa795af9d1c790df45fded1c2?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nlbwuwXz6OuJy8CYOgy6uSdsCiPMXfC7~uQN~-oOOl3hg6-0~PBAUJ7mQ-KC2R7jx9r~hl9eU7Rj1W2hDiUT4DedviUnM6pZkb1WPFAZPNX3OM5xjMVaVZ3uFcHIJ7I4u64B5HkmdOIhE7JdZHnCR5sSzM2vszVfgr0U~6FNQzIevYz~1VPB7e2bgR5f7fkwt0kj8ipHmwyBQ6uOIIj~VtKXLcb4rdD3I~yky-YbJqIGF5FSKnnRaLKSQh-m1XTiSJ2Q5E-KsHivGyPQ2iu4WiBcW9RDKlEB9sYk-5tgKq7Q0Eehx09w11SlW4XJ3mi-PpFlcisoz8pbthYFYS8S~Q__"
@@ -185,11 +94,11 @@ const AcademyPage = () => {
             alt="image"
             width={2400}
             height={800}
-            className="h-full w-full object-cover"
+            className="h-full w-full object-cover md:h-[400px]"
           />
         </div>
-        <div className="mx-auto flex h-fit w-full max-w-6xl flex-col justify-between px-4 md:mt-6 md:flex-row md:gap-5 md:px-10 xl:mt-8 xl:px-0">
-          <div className="content-info w-[400px]">
+        <div className="mx-auto mt-4 flex h-fit w-full max-w-6xl flex-col justify-between px-4 md:mt-0 md:flex-row md:gap-5 md:px-10 xl:mt-8 xl:px-0">
+          <div className="content-info md:w-[400px]">
             <span className="font-bold uppercase">Locate ASPA</span>
             <h2 className="big-heading text-2xl uppercase md:text-4xl">
               Your Gateway to Surfing Success Starts Here!
--- src/app/Components/home/Hero/HeroSection2.tsx
+++ src/app/Components/home/Hero/HeroSection2.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ContainerContent from "../../Container";
 import SliderPackage from "../../SliderPackage";
+import { ContentData } from "@/app/lib/Content";
 
 const HeroSection2 = () => {
+  const contentText = ContentData[0];
   return (
     <ContainerContent url="hero">
       <div className="hero-section-phase2 h-fit w-full space-y-6 px-4 py-8 text-center md:px-5 xl:px-0">
@@ -12,14 +14,7 @@ const HeroSection2 = () => {
               ASIAN SURF <br /> PERFORMANCE ACADEMY
             </h1>
             <p className="text-sm font-light xl:w-3/4 xl:text-base">
-              The Asian Surf Performance Academy (ASPA) is a high-performance
-              surf academy for all levels, from beginners to professionals. We
-              train national and foreign teams for competitions, supported by
-              experienced coaches with a proven track record. ASPA offers
-              private and group sessions covering coaching, analysis, physical
-              training, judging criteria, profile building, and mental
-              preparation. Whether you&apos;re starting out or aiming to go pro,
-              ASPA provides the tools and support to reach your full potential.
+              {contentText.description}
             </p>
           </div>
         </div>
